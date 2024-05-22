0cafb462c1ab18572dcc96619ff3174883e81a90
danyaosman <danya02011@hotmail.com>
2024-05-22 19:07:13 +0300

    HomeViewModel

--- app/src/main/java/com/example/lingo/userInterface/HomeViewModel.kt
+++ app/src/main/java/com/example/lingo/userInterface/HomeViewModel.kt
@@ -1,14 +1,8 @@
 package com.example.lingo.userInterface
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.lingo.Graph
 import com.example.lingo.repository.Repository
-import com.example.lingo.room.Course
-import com.example.lingo.room.User
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 
@@ -16,10 +10,8 @@ class HomeViewModel(
     private val repository: Repository,
     private val ioDispatcher: CoroutineDispatcher
 ):ViewModel(){
-
     fun getCourses(){
         viewModelScope.launch(ioDispatcher)
         {repository.getCourseNames()}
     }
-
 }
