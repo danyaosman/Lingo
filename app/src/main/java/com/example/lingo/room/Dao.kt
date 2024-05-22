5b11d23777c0f1ceb8be084142d9abcccc571232
shdab <130516923+shadatr@users.noreply.github.com>
2024-05-22 13:39:08 +0300

    initializedata added

--- app/src/main/java/com/example/lingo/room/Dao.kt
+++ app/src/main/java/com/example/lingo/room/Dao.kt
@@ -8,33 +8,39 @@ import androidx.room.Update
 
 @Dao
 interface UsersDao {
-    @Insert(onConflict=OnConflictStrategy.IGNORE)
-    suspend fun insert(user:User)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User)
 
-    @Query("SELECT * FROM users WHERE id=:id")
-    fun getUserById(id:Int):User
+    @Query("SELECT * FROM users_table WHERE username = :username")
+    fun getUser(username: String): User
+}
 
-    @Query("SELECT * FROM users WHERE username=:username")
-    fun getUserByName(username:String):User
+@Dao
+interface CourseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(course: Course)
+}
+
+@Dao
+interface LevelDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(level: Level)
+
+    @Query("SELECT * FROM levels_table WHERE courseId = :courseId")
+    fun getCourseLevels(courseId: Int): List<Level>
 }
 
 @Dao
 interface QuestionDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(question: Question)
-
-    @Query("SELECT * FROM questions WHERE levelId = :levelId AND courseId=:courseId")
-    fun getQuestionsByLvlAndCourse(levelId:Int, courseId: Int):List<Question>
 }
 
 @Dao
-interface LevelDao {
-
+interface QuestionListDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(level: Level)
-    @Update
-    suspend fun update(level:Level)
+    suspend fun insert(questionList: QuestionList)
 
-    @Query("SELECT * FROM levels WHERE courseid = :courseId")
-    fun getCourseLevels(courseId:Int):List<Boolean>
+    @Query("SELECT * FROM level_questions WHERE levelId = :levelId")
+    fun getQuestionlistByLvl(levelId: Int): QuestionList
 }
